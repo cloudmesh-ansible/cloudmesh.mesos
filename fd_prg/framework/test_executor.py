119b058adbc46ffdfa296e939af83ff1952d9f1e
Anurag Kumar Jain <anurag.jain.2301@gmail.com>
2017-04-22 01:55:58 -0400

    chg:usr: Updated face detection program

--- fd_prg/framework/test_executor.py
+++ fd_prg/framework/test_executor.py
@@ -95,7 +95,7 @@ class MyExecutor(mesos.interface.Executor):
                 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
                 for (x, y, w, h) in faces:
                     image = cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                    cv2.imwrite(name, image)
+                cv2.imwrite(name, image)
                 print len(faces)
                 return [True, len(faces)]
             except:
